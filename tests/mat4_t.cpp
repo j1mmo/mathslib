e5fc63779f6017f9da4981874955155e18e4af3d
j1mmo <jsherlockshaw@gmail.com>
2026-04-23 13:43:02 +0100

    adding test for operator[x,y] as well as unit test for that func

--- tests/mat4_t.cpp
+++ tests/mat4_t.cpp
@@ -30,6 +30,28 @@ TEST(mat4_t, single_indexing_operator_overloading) {
     ASSERT_EQ(m[15], 16);
 }
 
+TEST(mat4_t, double_indexing_operator_overloading) {
+  ASSERT_EQ((m[0, 0]), 1);
+  ASSERT_EQ((m[0, 1]), 2);
+  ASSERT_EQ((m[0, 2]), 3);
+  ASSERT_EQ((m[0, 3]), 4);
+  
+  ASSERT_EQ((m[1, 0]), 5);
+  ASSERT_EQ((m[1, 1]), 6);
+  ASSERT_EQ((m[1, 2]), 7);
+  ASSERT_EQ((m[1, 3]), 8);
+  
+  ASSERT_EQ((m[2, 0]), 9);
+  ASSERT_EQ((m[2, 1]), 10);
+  ASSERT_EQ((m[2, 2]), 11);
+  ASSERT_EQ((m[2, 3]), 12);
+  
+  ASSERT_EQ((m[3, 0]), 13);
+  ASSERT_EQ((m[3, 1]), 14);
+  ASSERT_EQ((m[3, 2]), 15);
+  ASSERT_EQ((m[3, 3]), 16);
+}
+
 TEST(mat4_t, single_indexing_operator_overloading_editable) {
     mat4 m{0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
     m[0]=1;
